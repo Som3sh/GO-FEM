0d67578803eab10369483d4e5e4650d911548ce9
Som3sh <someshmohanty08@gmail.com>
2024-06-10 20:20:28 +0530

    Collections updated

--- goProjOne/data/collections.go
+++ goProjOne/data/collections.go
@@ -4,12 +4,12 @@ var Country [10]string
 
 const MaxSpeed = 6000000
 
-// func init() {
-// 	Country[0] = "India"
-// 	Country[1] = "USA"
-// 	Country[2] = "UK"
-// 	Country[6] = "France"
-// 	Country[7] = "Italy"
-// 	Country[8] = "Japan"
-// 	Country[9] = "China"
-// }
+func init() {
+	Country[0] = "India"
+	Country[1] = "USA"
+	Country[2] = "UK"
+	Country[6] = "France"
+	Country[7] = "Italy"
+	Country[8] = "Japan"
+	Country[9] = "China"
+}
