25ea62ce659fa447eabd3947934291345e3f96f2
Som3sh <someshmohanty08@gmail.com>
2024-06-10 23:30:31 +0530

    created a new project for simple calculator

---
+++ Calculator/go.mod
@@ -0,0 +1,3 @@
+module calculator.com/go/calc
+
+go 1.21.6
--- goProjOne/main.go
+++ goProjOne/main.go
@@ -4,17 +4,22 @@ import (
 	"fmt"
 )
 
-func calculateTax(price float32) (float32, float32) {
-	return price * 0.09, price * 0.02
-}
-func calculateTaxTwo(price float32) (float32, float32) {
-	return price * 0.07, price * 0.05
-}
+// func calculateTax(price float32) (float32, float32) {
+// 	return price * 0.09, price * 0.02
+// }
+// func calculateTaxTwo(price float32) (float32, float32) {
+// 	return price * 0.07, price * 0.05
+// }
 
 // func init() {
 // 	fmt.Println("This is init function")
 
 // }
+func birthday(pointerAge *int) {
+	if *pointerAge > 50 {
+		panic("You are too old")
+	}
+}
 func main() {
 
 	// fmt.Println("fuck off")
@@ -23,10 +28,14 @@ func main() {
 	// // names := []string{"Mary", "John", "Casper"}
 	// // // names := append(names , "Carol")
 	// // println(len(names))
-	stateTax, cityTax := calculateTax(100)
-	fmt.Println(stateTax, cityTax)
+	// stateTax, cityTax := calculateTax(100)
+	// fmt.Println(stateTax, cityTax)
 
-	countryTax, _ := calculateTaxTwo(2000)
-	fmt.Println(countryTax)
+	// countryTax, _ := calculateTaxTwo(2000)
+	// fmt.Println(countryTax)
+	defer fmt.Println("Bye")
+	age := 23
+	birthday(&age)
+	fmt.Println(age)
 
 }
