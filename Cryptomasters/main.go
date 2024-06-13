15b543268e85df515d7792fa64ad3b7a045627f4
Som3sh <someshmohanty08@gmail.com>
2024-06-13 12:22:09 +0530

    editted the code a bit and made the currencies user input and multiple currencies at the same time

--- Cryptomasters/main.go
+++ Cryptomasters/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+
+	"learninggo.com/go/Cryptomasters/api"
+)
+
 func main() {
+	getCurrencyData()
+
+}
+
+func getCurrencyData() {
+	var currency string
+	var n int
+	fmt.Print("How many currencies do you want to check? Enter : \t")
+
+	fmt.Scanf("%d", &n)
+	for i := 0; i < n; i++ {
+		fmt.Print("\nEnter the currency you want to check : \t")
+		fmt.Scanf("%s", &currency)
+
+		rate, err := api.GetRate(currency)
+		if err != nil {
+			fmt.Println(err)
+
+		} else {
+
+			fmt.Printf("The rate for %v is %.2f\n", rate.Currency, rate.Price)
+			fmt.Println("---------------------------")
+		}
+	}
+	fmt.Println("\n\n------------Thank You---------------")
 
 }
